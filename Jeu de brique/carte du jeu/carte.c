9221c9b08bebe14e361df7ee47290e9b445d827c
AGUIR Chams <109348212+AguirChams@users.noreply.github.com>
2024-09-19 16:59:45 +0200

    Update carte.c

--- Jeu de brique/carte du jeu/carte.c
+++ Jeu de brique/carte du jeu/carte.c
@@ -4,35 +4,36 @@
 #define MUR 1
 #define BRIQUE 2
 typedef struct{
-    int **grille;  // Grille de la carte x*x
+    int **hauteur;  // tab de pointeurs hauteur de la carte x*x
+    int **largeur;  // tab de pointeurs largeur de la carte x*x
     int taille;   // Taille de la carte x*x
 }t_carte;
 // Création d'une carte
-t_carte* creer_carte(int taille){
+t_carte** creer_carte(int taille){
     int i,j;
-    t_carte *carte = (t_carte*) malloc(sizeof(t_carte));
-    carte->taille = taille;
-    //creation d'un pointeur vers un tableau d'entier de taille x qui represente la grille
-    carte->grille = (int**) malloc(taille * sizeof(int*));
-    //creation d'un pointeur vers un tableau d'entier qui represente la ligne de la carte et qui doit etre de taille x
+
+ 
+    //creation d'un pointeur vers un tableau d'hauteur de taille x qui represente la grille
+    int** hauteur = (int**) malloc(taille * sizeof(int*));
+    //creation d'un pointeur vers un tableau de hauteur qui represente la ligne de la carte et qui doit etre de taille x
     for (i = 0; i < taille; i++) {
-        carte->grille[i] = (int*)malloc(taille * sizeof(int));
+        hauteur[i] = (int*)malloc(taille * sizeof(int));
     }
     //remplissage de la carte avec des murs et des briques
     for(i=0; i< taille; i++){
         for(j=0; j<taille; j++){
             //initialisation des murs autour du bord
             if(i==0|| i==taille-1 || j==0|| j==taille-1){
-                carte->grille[i][j] = MUR;
+                hauteur[i][j] = MUR;
             }else{
-                carte->grille[i][j] = VIDE;
+                hauteur[i][j] = VIDE;
             }
         }
     }
-    return carte;
+    return hauteur;
 }
 // recuperation d'un element de la carte
-int recuperer_element(t_carte *carte, int x, int y){
+/*int recuperer_element(t_carte *carte, int x, int y){
     if(x<0 || x>=carte->taille || y<0 || y>=carte->taille){
         printf("Erreur : coordonnees invalides\n");
         return -1;
@@ -61,14 +62,20 @@ void liberer_carte(t_carte *carte){
     }
     free(carte->grille);
     free(carte);
-}
+}*/
 int main(){
-    int taille=5;
-    t_carte *carte = creer_carte(taille);
-    mettre_a_jour_element(carte, 2, 2, BRIQUE);
+    int taille=1;
+    int** hauteur = creer_carte(taille);
+    for(int i=0; i<taille; i++){
+        for(int j=0; j<taille; j++){
+            printf("%d ", hauteur[i][j]);
+        }
+        printf("\n");
+    }
+    /*mettre_a_jour_element(carte, 2, 2, BRIQUE);
     mettre_a_jour_element(carte, 4, 2, BRIQUE);
     afficher_carte(carte);
-    liberer_carte(carte);
+    liberer_carte(carte);*/
     return 0;
 }
 
